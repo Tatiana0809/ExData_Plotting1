3f2139bc8e5421741f2c44661d898879572e4619
Tatiana <tatiyana.baltyzhakova@gmail.com>
2015-05-09 19:01:17 +0300

    Create plot1.r

---
+++ plot1.r
@@ -0,0 +1,2 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+"c:\\Users\\пользователь\\Downloads\\d396qusza40orc.cloudfront.net/exdata-Fdata-Fhousehold_power_consumption.zip")
