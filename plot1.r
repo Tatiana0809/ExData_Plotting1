562df8262f83cadaa209ddc23db1db98f39eecfa
Tatiana <tatiyana.baltyzhakova@gmail.com>
2015-05-09 20:02:27 +0300

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,2 +1,9 @@
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-"c:\\Users\\пользователь\\Downloads\\d396qusza40orc.cloudfront.net/exdata-Fdata-Fhousehold_power_consumption.zip")
+#reading the data from the file
+#data for 2007-02-01 and 2007-02-02 was previously subsetted into the file
+d<-read.table("data_power_consumption.txt",header=TRUE,sep=",")
+active_power<-as.numeric(d$Global_active_power)
+#creating png file
+png("Plot1.png",width=480,height=480)
+#plotting histogramm
+hist(active_power,col="red",main="Global Active Power",xlab="Global active power (kilowatts)")
+dev.off()
